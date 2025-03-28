59f5a8df456e0c224a51fac60816d6adbbcc5fc1
murali-ganta <muralireddy2910@gmail.com>
2025-03-27 20:50:49 -0400

    Models Added

---
+++ Models/CityModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace WeatherApp.Models
+{
+    // Represents a city with details such as its name and country code.
+    // This class is useful for storing city-specific information when searching for weather data.
+    public class CityModel
+    {
+        public string CityName { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string Region { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public int Population { get; set; }
+    }
+}
---
+++ Models/WeatherModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+
+    //  Represents the weather data for a given city.
+    // This model contains details such as temperature, humidity, weather condition, and wind speed.
+    public class WeatherModel
+    {
+        public float Temperature { get; set; }
+
+        public int Humidity { get; set; }
+
+        public string Condition { get; set; }
+
+        public float WindSpeed { get; set; }
+
+        public float pressure { get; set; }
+
+        public DateTime ReportTime { get; set; }
+    }
+}
